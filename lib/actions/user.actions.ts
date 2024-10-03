d393355e55b7c0f5271b57cf606b8bd18c440a0e
Kavindu Madushanka <kavindumadushanka972@gmail.com>
2024-10-03 11:52:14 +0530

    Feat: mention users

--- lib/actions/user.actions.ts
+++ lib/actions/user.actions.ts
@@ -2,6 +2,7 @@
 
 import { clerkClient } from "@clerk/nextjs/server";
 import { parseStringify } from "../utils";
+import { liveblocks } from "../liveBlocks";
 
 export const getClerkUsers = async ({userIds}: {userIds: string[]}) => {
   try {
@@ -21,6 +22,27 @@ export const getClerkUsers = async ({userIds}: {userIds: string[]}) => {
 
     return parseStringify(sortedUsers);
 
+  } catch (error) {
+    console.log(`Error happened while fetching users: ${error}`);
+  }
+}
+
+export const getDocumentUsers = async ({roomId, currentUser, text}: {
+  roomId: string;
+  currentUser: string;
+  text: string;
+}) => {
+  try {
+    const room = await liveblocks.getRoom(roomId);
+    const users = Object.keys(room.usersAccesses).filter((email) => email !== currentUser);
+
+    if(text.length) {
+      const lowerCaseText = text.toLowerCase();
+      const filteredUsers = users.filter((email: string) => email.toLowerCase().includes(lowerCaseText));
+      return parseStringify(filteredUsers); // can mention only filtered users by text
+    }
+
+    return parseStringify(users); // can mention anyone
   } catch (error) {
     console.log(`Error happened while fetching users: ${error}`);
   }
